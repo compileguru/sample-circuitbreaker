2fa0ab9334a100c8ff9687b37c187e28409d3bda
compileguru <gkumar.upload@gmail.com>
2025-03-28 10:17:56 +0530

    Adding postman collection

--- src/main/java/com/compileguru/circuitbreaker/client/ExternalServiceClient.java
+++ src/main/java/com/compileguru/circuitbreaker/client/ExternalServiceClient.java
@@ -2,7 +2,7 @@ package com.compileguru.circuitbreaker.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//To test failure and retry mechanism uncomment the line below
 @FeignClient(name = "external-service", url = "https://dummyjson.com")
 //@FeignClient(name = "external-service", url = "https://localhost:9999")
 public interface ExternalServiceClient {
